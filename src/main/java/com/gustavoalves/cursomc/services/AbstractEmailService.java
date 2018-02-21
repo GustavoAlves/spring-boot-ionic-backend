2d0170d8fc5bcae85d83861a55288f49e216fa92
Gustavo Alves <gustavo_alves@outlook.com>
2018-02-20 23:02:10 -0300

    Esqueci a senha

--- src/main/java/com/gustavoalves/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/gustavoalves/cursomc/services/AbstractEmailService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.gustavoalves.cursomc.domain.Cliente;
 import com.gustavoalves.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -16,7 +17,6 @@ public abstract class AbstractEmailService implements EmailService {
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
-		
 	}
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
@@ -27,6 +27,24 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
+		
+		return sm;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha:" + newPass);
 		
 		return sm;
 	}
--- src/main/java/com/gustavoalves/cursomc/services/EmailService.java
+++ src/main/java/com/gustavoalves/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.gustavoalves.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.gustavoalves.cursomc.domain.Cliente;
 import com.gustavoalves.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,5 +10,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
