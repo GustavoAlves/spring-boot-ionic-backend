15502c38458df62cdfed623122c0f435e78bfcca
Gustavo Alves <gustavo_alves@outlook.com>
2018-03-11 14:00:21 -0300

    Selecionando pacote do projeto e adicionando informações personalizadas à documentação

--- src/main/java/com/gustavoalves/cursomc/config/SwaggerConfig.java
+++ src/main/java/com/gustavoalves/cursomc/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.gustavoalves.cursomc.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,11 +17,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	@Bean public Docket api() {
+	@Bean
+	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.gustavoalves.cursomc.resources"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(apiInfo());
 	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfo("API do curso Spring Boot",
+				"Esta API é utilizada no curso de Spring Boot do prof. Nelio Alves", 
+				"Versão 1.0",
+				"https://www.udemy.com/terms",
+				new Contact("Gustavo Alves", "linkedin.com/in/gusalves/", "gustavo_alves@outlook.com"),
+				"Permitido uso para estudantes", 
+				"https://www.udemy.com/terms", 
+				Collections.emptyList() // Vendor																					// Extensions
+		);
+}
 }
